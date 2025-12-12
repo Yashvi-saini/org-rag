761b128542e2e0ea064efc85e27476729076a956
Yashvi-saini <yashvisaini.ys@gmail.com>
2025-12-12 11:59:03 +0530

    layout fixed

--- src/components/auth/layout/AuthLayout.tsx
+++ src/components/auth/layout/AuthLayout.tsx
@@ -16,11 +16,10 @@ export default function AuthLayout({ children, images, contentTop = 30, showCont
   const router = useRouter();
   return (
     <div
-      className={`${poppins.className} flex w-[1439px] mx-auto min-h-screen bg-[#003259]`}
+      className={`${poppins.className} flex w-screen min-h-screen bg-[#003259]`}
     >
       {/* Left Carousel */}
-      <div className="hidden md:flex relative w-[628px] bg-[#003259] h-[100vh]">
-        {/* Top back arrow (common across pages) */}
+      <div className="hidden md:flex relative w-[45%] bg-[#003259] h-screen">
         <button
           type="button"
           aria-label="Go back"
@@ -29,20 +28,16 @@ export default function AuthLayout({ children, images, contentTop = 30, showCont
         >
           <Image src="/auth/topbackarrow.svg" alt="back" width={24} height={24} />
         </button>
+
         <Carousel images={images} showControls={showControls} />
       </div>
 
-      {/* Right Form  */}
-      <div className="w-[811px] flex flex-col items-center bg-white pt-[42px] pb-[234px] overflow-y-auto">
-        
-        {/* Logo */}
-        <div>
-          <Image src="/DocClustor.svg" alt="logo" width={169} height={24} />
-        </div>
+      {/* Right Form */}
+      <div className="flex-1 flex flex-col items-center bg-white pt-[42px] pb-[234px] overflow-y-auto ">
+        <Image src="/DocClustor.svg" alt="logo" width={169} height={24} />
 
-        {/* Form container */}
         <div
-          className="w-[610px] flex flex-col gap-[30px]"
+          className="w-full max-w-[610px] flex flex-col gap-[30px]"
           style={{ marginTop: contentTop }}
         >
           {children}
